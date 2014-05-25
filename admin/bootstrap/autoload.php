a7dbe4f50a158e7c77ac6220803b3e058ceccb61
= <=>
2014-05-25 13:21:51 -0300

    Ajustes de configuração da estrutura

--- admin/bootstrap/autoload.php
+++ admin/bootstrap/autoload.php
@@ -11,5 +11,14 @@ define('BEWOH_START', microtime(true));
 */
 require __DIR__.'/../vendor/autoload.php';
 
+/*
+|--------------------------------------------------------------------------
+| Adiciona os diretórios da estrutura
+|--------------------------------------------------------------------------
+|
+| Nessa etapa o objeto ClassLoader adiciona os diretórios necessários para a
+| estrutura.
+|
+*/
 \beowh\ClassLoader::addDirectories(require __DIR__.'/arquivos.php');
 \beowh\ClassLoader::register();
